ba0275055232bf8795d1a3bf3bd309c23d827625
Dylan Williams <happyfrog578+github@gmail.com>
2020-09-07 19:11:03 +1000

    basic commands added

--- bot.rb
+++ bot.rb
@@ -1,8 +1,27 @@
 require 'discordrb'
+require 'yaml'
 
-@my_bot = Discordrb::Bot.new token: <todo>, client_id <todo>
-@my_bot.run true
+CONFIG = YAML.load_file('config.yaml')
+bot = Discordrb::Bot.new token: CONFIG['token']
 
-#commands go here
+bot.message(with_text: 'oi') do |event|
+  event.respond 'What dog'
+end
 
-@my_bot.join
+bot.message(with_text: 'cunt') do |event|
+  event.respond 'fucken dog cunt ay'
+end
+
+bot.message(with_text: 'rekt') do |event|
+  event.respond "rekt\nrekt\nrekt\nrekt\nrekt\nrekt\nrekt\nrekt\nrekt\nrekt\nrekt\nrekt\nrekt\nrekt\n"
+end
+
+bot.message(with_text: 'goodboy') do |event|
+  event.respond ":KeelanGoodboy:"
+end
+
+bot.message(with_text: 'nah') do |event|
+  event.respond "nah nah nah nah nah nah nah nah nah"
+end
+
+bot.run
