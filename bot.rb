fa9e9c505cb8a4f4fe30f48abf9416d6d775a281
Dylan Williams <happyfrog578+github@gmail.com>
2020-09-08 00:20:21 +1000

    glorious robo keelan evolution. gitignore for yaml

--- bot.rb
+++ bot.rb
@@ -5,7 +5,8 @@ CONFIG = YAML.load_file('config.yaml')
 bot = Discordrb::Bot.new token: CONFIG['token']
 
 bot.message(with_text: 'oi') do |event|
-  event.respond 'What dog'
+  event.respond "What dog"
+  event.respond "<:UWot:311362042386972672>"
 end
 
 bot.message(with_text: 'cunt') do |event|
@@ -17,11 +18,23 @@ bot.message(with_text: 'rekt') do |event|
 end
 
 bot.message(with_text: 'goodboy') do |event|
-  event.respond ":KeelanGoodboy:"
+  event.respond "<:KeelanGoodboy:311362006638919680>"
+end
+
+bot.message(with_text: 'goodboys') do |event|
+  5.times {|_goodboy| event.respond "<:KeelanGoodboy:311362006638919680>" }
 end
 
 bot.message(with_text: 'nah') do |event|
   event.respond "nah nah nah nah nah nah nah nah nah"
+end
+
+bot.message(with_text: 'vb') do |event|
+  event.respond "https://media.discordapp.net/attachments/131257164235800576/644429935733374979/happy_morgsmk3.gif"
+end
+
+bot.message(with_text: 'suck') do |event|
+  event.respond "SUCK EM DRY"
 end
 
 bot.run
